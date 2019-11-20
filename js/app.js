4ae9c879d43cc9de4457ab85de573ae0bb1fe915
marccharton <kramoogle@gmail.com>
2019-11-20 11:35:18 +0100

    feat: first use of react in app, need to configure react-redux.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
-import redux from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import redux from 'redux';
+import { configureStore, createAction, createSlice } from '@reduxjs/toolkit';
+import App from './components/App.jsx';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('cb-app')
-);
+const counterSlice = createSlice({
+	name: 'counter',
+	initialState: 0,
+	reducers: {
+    increment: (state) => state + 1,
+		decrement: (state) => state - 1
+	}
+});
+const { actions, reducer } = counterSlice;
+
+const store = configureStore({
+	reducer: reducer
+});
+
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+const { increment, decrement } = actions;
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(increment());
+
+ReactDOM.render(<App />, document.getElementById('cb-app'));
